3a1764ca563cf348c3a983312c39e6087c83f7e9
Dalen <dalen2020@gmail.com>
2020-06-22 22:06:09 +0000

    Creates new servlet files.

---
+++ src/main/java/com/google/sps/servlets/new_commnent.java
@@ -0,0 +1,15 @@
+package com.google.sps.servlets;
+
+import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Webservlet ("/new_comment")
+public class new_comment extends HttpServlet{    
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException{
+
+    }
+}
