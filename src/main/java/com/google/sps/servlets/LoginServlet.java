241312536149b1a27c2b02bdf8345ef27b912296
Nikita <jindal@college.harvard.edu>
2020-06-23 22:49:00 +0000

    Added functionality for login and loading the representative list

--- src/main/java/com/google/sps/servlets/LoginServlet.java
+++ src/main/java/com/google/sps/servlets/LoginServlet.java
@@ -15,6 +15,6 @@ public class LoginServlet extends HttpServlet{
     
     @Override
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException{
-
+        
     }
 }
--- src/main/java/com/google/sps/servlets/RepListServlet.java
+++ src/main/java/com/google/sps/servlets/RepListServlet.java
@@ -5,11 +5,61 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Scanner;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.util.EntityUtils;
+import org.apache.http.HttpEntity;
+import java.net.URISyntaxException;
+import java.net.URI;
+import org.apache.http.impl.client.HttpClients;
+import com.google.gson.Gson;
 
 @WebServlet ("/rep_list")
 public class RepListServlet extends HttpServlet{
+
+    private final String API_KEY = "AIzaSyAHQRFZpZBZSOeUI-4pmfoHV4SM0eBSlS4";
+
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException{
+        String zipcode = request.getParameter("zipcode");
+        HttpClient httpclient = HttpClients.createDefault();
+
+        URIBuilder builder = new URIBuilder();
+        builder.setScheme("https").setHost("www.googleapis.com/civicinfo/v2/representatives")
+        .setParameter("key", API_KEY)
+        .setParameter("address", zipcode);
+        URI uri = null;
+        try{
+            uri = builder.build();
+        } catch(URISyntaxException e){
+            //print some shit
+        }
+        HttpGet httpget = new HttpGet(uri);
+
+        HttpResponse httpresponse = null;
+        String responseString = null;
+
+        try {
+            httpresponse = httpclient.execute(httpget);
+        } catch (IOException e) {
+            //handle this IOException properly in the future
+        } catch (Exception e) {
+            //handle this IOException properly in the future
+        }
+
+        HttpEntity responseEntity = httpresponse.getEntity();
+        if(responseEntity != null) {
+            responseString = EntityUtils.toString(responseEntity);
+        }
+        
+        System.out.println(responseString);
+        String json = new Gson().toJson(responseString);
+        response.setContentType("application/json");
+        response.getWriter().println(json);
+
 
     }
 }
